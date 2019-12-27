bf22c81260a14b212e957efd0c38670d1274b18a
Ante Turudic <ante.turudic@gmail.com>
2019-12-27 16:02:17 +0100

    Renamed: bio -> zci_bio

--- bin/zcit.py
+++ bin/zcit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from zcitools.zcit import ZCIT
-from bio import registered_commands, registered_steps
+from zci_bio import registered_commands, registered_steps
 
 # Note: this script is called from project main directory, all used filenames are relative to it!
 zcit = ZCIT(registered_commands=registered_commands, registered_steps=registered_steps)
