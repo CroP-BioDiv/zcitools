bbc2cd889e09588caa59e255e9fe852a503c7bcd
Ante Turudic <ante.turudic@gmail.com>
2020-03-02 17:59:50 +0100

    CommonDB commands cleanup

--- step_project/common/common_db_commands.py
+++ step_project/common/common_db_commands.py
@@ -3,6 +3,7 @@ from zipfile import ZipFile
 from ..base_commands import NonProjectCommand
 from common_utils.common_db import CommonDB, SEQUENCE_DBS_RELATIVE_DIR
 from common_utils.terminal_layout import StringColumns
+from common_utils.file_utils import read_file_as_list, silent_remove_file
 
 
 def convert_bytes(num):
@@ -19,30 +20,35 @@ class _CommonDBCommand(NonProjectCommand):
     def set_arguments(parser):
         parser.add_argument('path', help='CommonDB path')
         parser.add_argument('-p', '--filter-path', help='Filter path')
-        parser.add_argument('-x', '--recursive', action='store_true', help='Search recursively given path')
+        parser.add_argument('-d', '--only-directory', action='store_true', help='Search recursively given path')
         parser.add_argument(
             '-r', '--record', help='Search all occurences of a record(s) in given path. Format <rec>,<rec>,...')
+        parser.add_argument(
+            '--records-filename', help='Search all occurences of a records read from given file.')
         dbs = CommonDB.get_zci_sequence_dbs()
         parser.add_argument('-S', '--sequence-db', help=f'Sequence database to use: {", ".join(dbs)}')
 
     def _iterate_records(self):
         # Returns records as tuples (relative_dir, record ident)
-        records = self.args.record.split(',') if self.args.record else None
-        n = len(self._db_dir) + 1
-        if self.args.recursive or records:
+
+        # Find records to filter
+        records = set()
+        if self.args.record:
+            records.update(self.args.record.split(','))
+        if self.args.records_filename:
+            records.update(read_file_as_list(self.records_filename))
+
+        #
+        if self.args.only_directory:
+            yield from ((None, f) for f in os.listdir(self._db_dir)
+                        if (not records or f in records) and os.path.isfile(os.path.join(self._db_dir, f)))
+        else:
             filter_path = self.args.filter_path
+            n = len(self._db_dir) + 1
             for root, subdirs, files in os.walk(self._db_dir):
                 rel_dir = root[n:]
-                if filter_path and filter_path not in rel_dir:
-                    continue
-                for f in files:
-                    if records and f not in records:
-                        continue
-                    yield (rel_dir, f)
-        else:
-            for f in os.listdir(self._db_dir):
-                if os.path.isfile(os.path.join(self._db_dir, f)):
-                    yield (None, f)
+                if not filter_path or filter_path in rel_dir:
+                    yield from ((rel_dir, f) for f in files if not records or f in records)
 
     def _db_file(self, rel_dir, record):
         if rel_dir:
@@ -71,20 +77,46 @@ class CommonDBList(_CommonDBCommand):
     def set_arguments(parser):
         _CommonDBCommand.set_arguments(parser)
         parser.add_argument('-a', '--all-data', action='store_true', help='Print all data inside zip file')
+        parser.add_argument('-1', '--only-names', action='store_true', help='Print only names one per a line')
 
     def _run(self):
-        rows = []
-        for rel_dir, record in self._iterate_records():
-            zip_filename = self._db_file(rel_dir, record)
-            with ZipFile(zip_filename, 'r') as zip_f:
-                zip_files = ', '.join(f"{z_i.filename} ({convert_bytes(z_i.file_size)})"
-                                      for z_i in zip_f.infolist() if not z_i.is_dir())
-            rows.append([record, rel_dir, convert_bytes(os.path.getsize(zip_filename)), zip_files])
-        print(StringColumns(rows, header=['Record', 'Dir', 'Size', 'Data']))
+        # How to print
+        if self.args.only_names:
+            header = None
+
+            def _row(record, rel_dir, zip_filename):
+                return [record]
+
+        elif self.args.all_data:
+            header = ['Record', 'Dir', 'Size', 'Data']
+
+            def _row(record, rel_dir, zip_filename):
+                with ZipFile(zip_filename, 'r') as zip_f:
+                    zip_files = ', '.join(f"{z_i.filename} ({convert_bytes(z_i.file_size)})"
+                                          for z_i in zip_f.infolist() if not z_i.is_dir())
+                return [record, rel_dir or '', convert_bytes(os.path.getsize(zip_filename)), zip_files]
+
+        else:
+            header = ['Record', 'Dir', 'Size']
+
+            def _row(record, rel_dir, zip_filename):
+                return [record, rel_dir or '', convert_bytes(os.path.getsize(zip_filename))]
+
+        #
+        rows = [_row(record, rel_dir, self._db_file(rel_dir, record)) for rel_dir, record in self._iterate_records()]
+
+        if rows:
+            if self.args.only_names:
+                for r in rows:
+                    print(r[0])
+            else:
+                print(StringColumns(rows, header=header))
+        else:
+            print('No records found!')
 
 
-class CommonDBExtract(_CommonDBCommand):
-    _COMMAND = 'cdb_extract'
+class CommonDBGet(_CommonDBCommand):
+    _COMMAND = 'cdb_get'
     _HELP = "Extract CommonDB record(s) from given path"
 
     def _run(self):
@@ -104,4 +136,5 @@ class CommonDBRemove(_CommonDBCommand):
     _HELP = "Remove CommonDB record(s)"
 
     def _run(self):
-        print('ToDo: remove')
+        for rel_dir, record in self._iterate_records():
+            silent_remove_file(self._db_file(rel_dir, record))
