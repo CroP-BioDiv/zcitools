af76bd7e5ede620ef0b50f9916ed0324dce585a3
Ante Turudic <ante.turudic@gmail.com>
2020-04-18 19:14:02 +0200

    Added check_PATH_conflicts.py

---
+++ bin/check_PATH_conflicts.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import os
+from collections import defaultdict
+
+exe_2_path = defaultdict(list)
+processed_paths = set()
+for path in os.environ.get('PATH', '').split(os.pathsep):
+    if path not in processed_paths:
+        processed_paths.add(path)
+        if os.path.isdir(path):
+            for f in os.listdir(path):
+                fp = os.path.join(path, f)
+                if os.path.isfile(fp) and os.access(fp, os.X_OK):
+                    exe_2_path[f].append(path)
+
+for exe, paths in sorted(exe_2_path.items()):
+    if len(paths) > 1:
+        print(f"{exe:<20} : {', '.join(paths)}")
