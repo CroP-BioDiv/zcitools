40093cb5781ffe894d02482f0258213ec77d16da
Ante Turudic <ante.turudic@gmail.com>
2020-12-28 00:10:42 +0100

    run_all_steps cleanup

--- bin/run_all_steps.py
+++ bin/run_all_steps.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 import os
+import sys
 import re
 import subprocess
 pattern = re.compile(r'run_.*\.py')
 
-for d in os.listdir('.'):
+_dirs = sys.argv[1:] if len(sys.argv) > 1 else os.listdir('.')
+
+for d in _dirs:
     if os.path.isdir(d) and not os.path.exists(os.path.join(d, 'output.zip')):
         py_files = [f for f in os.listdir(d) if pattern.search(f)]
         if len(py_files) == 1:
